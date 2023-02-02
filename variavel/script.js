9b1cda464dad38f6e296bafbf54a48128710346b
RodrigoFN <107443321+Rodrigo-FN@users.noreply.github.com>
2023-02-01 22:42:38 -0300

    guia estelar

---
+++ variavel/script.js
@@ -0,0 +1,18 @@
+// var
+var clima = "O clima está quente!<br>"
+clima = "Frio <br>"  // Pode ser reatribuido em qualquer momento
+
+document.write(clima)
+
+// Let
+let climaFrio = "O clima está frio!<br>"
+climaFrio = "quente <br>"  // Pode ser reatribuido dentro de blocos
+document.write(climaFrio)
+
+// Const
+const Chuva = "Está chuvendo muito!"
+Chuva = "Sol"               // Não pode ser reatribuido em momento algum
+document.write(Chuva)
+
+// var é global funciona no escopo da pagína
+// Const e let são locais e só funciona no escopo onde foi criada
