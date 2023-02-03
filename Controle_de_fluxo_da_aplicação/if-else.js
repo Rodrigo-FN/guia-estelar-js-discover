b511864258f7e72a6207e41f83a604955ea5bea2
RodrigoFN <107443321+Rodrigo-FN@users.noreply.github.com>
2023-02-03 14:34:35 -0300

    Controle de fluxo da aplicação

---
+++ Controle_de_fluxo_da_aplicação/if-else.js
@@ -0,0 +1,13 @@
+// if...else
+
+let temperature = 36
+let highTemperature = 37.5
+let mediumTemperature = 37
+
+if(temperature >= highTemperature) {
+    console.log("febre alta")
+}else if (temperature >= mediumTemperature){
+    console.log('febre moderada')
+}else {
+    console.log('Saúdavel')
+}
